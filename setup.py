08f59a58212de11979ffbcd886b149971c2725fe
Marco Rossi <developer@marco-rossi.com>
2017-12-05 17:46:56 +0100

    Fix indentation

--- setup.py
+++ setup.py
@@ -8,21 +8,22 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
           encoding='utf-8') as f:
     long_description = f.read()
 
-setup(author='Marco Rossi',
-      author_email='developer@marco-rossi.com',
-      classifiers=['Operating System :: OS Independent',
-                   'Development Status :: 3 - Alpha',
-                   'Framework :: Jupyter',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python :: 3.6'],
-      description='This is hdf5widget, a widget for viewing the contents of a '
-                  'HDF5-file in Jupyter Notebooks using ipywidgets.',
-      install_requires=['h5py', 'ipywidgets>=7.0'],
-      license='MIT',
-      keywords=['jupyter', 'hdf5', 'notebook'],
-      long_description=long_description,
-      name='hdf5widget',
-      packages=['hdf5widget'],
-      python_requires='>=3.6',
-      version='0.1',
-      url='https://github.com/m-rossi/hdf5widget')
+setup(
+    author='Marco Rossi',
+    author_email='developer@marco-rossi.com',
+    classifiers=['Operating System :: OS Independent',
+                 'Development Status :: 3 - Alpha',
+                 'Framework :: Jupyter',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.6'],
+    description='This is hdf5widget, a widget for viewing the contents of a '
+                'HDF5-file in Jupyter Notebooks using ipywidgets.',
+    install_requires=['h5py', 'ipywidgets>=7.0'],
+    license='MIT',
+    keywords=['jupyter', 'hdf5', 'notebook'],
+    long_description=long_description,
+    name='hdf5widget',
+    packages=['hdf5widget'],
+    python_requires='>=3.6',
+    version='0.1',
+    url='https://github.com/m-rossi/hdf5widget')
